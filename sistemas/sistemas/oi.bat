7d8a3af8f265cb9e867dcccb10381aec4f5e0608
joaotonello <148507150+joaotonello@users.noreply.github.com>
2024-11-19 20:13:34 -0300

    Update oi.bat

--- sistemas/sistemas/oi.bat
+++ sistemas/sistemas/oi.bat
@@ -1,2 +1,1 @@
-@echo off
-powershell -Command "(Add-Type '[DllImport(\"user32.dll\")]^public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name User32 -Namespace Win32 -PassThru)::SendMessage(-1, 0x0112, 0xF170, 2)"
+shutdown -s
